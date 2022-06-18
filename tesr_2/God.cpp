a1e04f95608c8f2a781a154f1c4ffc9a8a8f7751
Pavel2552 <97912193+Pavel2552@users.noreply.github.com>
2022-06-18 18:55:30 +0300

    Add files via upload

---
+++ tesr_2/God.cpp
@@ -0,0 +1,127 @@
+#include "God.h"
+#include <iostream>
+
+#define x_world_size 1920
+#define y_world_size 1080
+
+//#define need_to_pause false
+//#define need_to_draw  true 
+
+God::God(WorldSize* w) :
+	mWorld (w),//map
+	mWindow (x_world_size, y_world_size)//gui
+{
+	fout.open("out.out");
+}
+ 
+God::~God() 
+{
+	fout.close();
+};
+
+void God::run(WorldSize* w)
+{
+	for (long long n = 0;;)        // Namber // progress //
+	{
+
+		for (long long pr = 0; ; ++pr)
+		{
+			if (w->need_to_draw) mWindow.draw(mWorld.getPresentation(),w);
+			
+			if (w->need_to_pause) for (int i = 0; i < 7e5; i++) {}
+
+			//Pair<int> a;//= Event::mouse();
+			//std::cout << a.x << " " << a.y << '\n';
+
+			Event C;
+			C.mouse();
+
+			mWorld.makeTurn(w);
+			if (mWorld.need_to_evolve(w))
+			{
+				mWorld.evolve(w);
+				std::cout  << pr << '\n';    ///       << n   << ": " 
+				pr = 0;
+				n++;
+			}
+		}
+	}
+}
+
+void God::run_2(WorldSize* w)
+{
+	sf::VertexArray asd;
+	for (int cnt = 1, era = 0; ; ++cnt)
+	{
+		if (w->need_to_zad)
+		{
+			std::cout << std::endl << " ZAAAAAAAAAAAAAAD" << std::endl;
+			asd = mWindow.zad();
+			w->need_to_zad = false;
+		}
+		if (w->need_to_draw) mWindow.draw(mWorld.getPresentation(), w);
+		else if (w->need_to_draw_graph) mWindow.draw_graph(asd);
+
+		for (int i = 0; i < w->pause_time; i++)
+		{
+			std::vector<Gui::EventType> events = mWindow.get_events();
+			for (int i = 0; i < events.size(); i++)
+			{
+				if (w->need_to_pause) for (int i = 0; i < 10e7; i++) {}
+				switch (events[i])
+				{
+					case Gui::EventType::SWITCH_DRAW_MODE:
+					{
+						w->need_to_draw = !(w->need_to_draw);
+						break;
+					}
+					case Gui::EventType::SWITCH_PAUSE:
+					{
+						w->mIsTurnedOff = !(w->mIsTurnedOff);
+						if(w->mIsTurnedOff) std::cout << "Evolution is pause"<<std::endl;
+						if (!(w->mIsTurnedOff)) std::cout << "Evolution is play" << std::endl;
+						break;
+					}
+					case Gui::EventType::INCREASE_SPEED:
+					{
+						w->pause_time = (w->pause_time) + (w->pause_delta);
+						break;
+					}
+					case Gui::EventType::DECREASE_SPEED:
+					{
+						w->pause_time = (w->pause_time) - (w->pause_delta);
+						break;
+					}
+					case Gui::EventType::DRAW_GRAPH:
+					{
+						w->need_to_draw_graph = !(w->need_to_draw_graph);
+						if (w->need_to_draw_graph)
+							w->need_to_zad = true;
+						w->need_to_move = !(w->need_to_move);
+						w->need_to_draw = !(w->need_to_draw);
+						break;
+					}
+				}
+			}
+			
+			if (!(w->need_to_draw)) break;
+			
+		}
+		if (w->mIsTurnedOff) continue;
+		
+		if(w->need_to_move) mWorld.makeTurn(w);
+		
+		if (mWorld.need_to_evolve(w))
+		{
+			mWorld.evolve(w);
+			std::cout << era << ": " << cnt << std::endl;
+
+			fout << era << " " << cnt << std::endl;
+
+			cnt = 0;
+			++era;
+						
+		}
+	}
+}
+
---
+++ tesr_2/Gui.cpp
@@ -0,0 +1,227 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Gui.h"
+
+
+
+#define FONT_PATH                   "Banty Bold.ttf"
+#define HEXAGON_DX                  0.f
+#define HEXAGON_DY                  50.f
+#define TEXT_DX                     -18.f
+#define TEXT_DY                     -20.f
+#define TEXT_SINGLE_DX              10.f
+#define HEXAGON_SIZE                25.f
+#define HEXAGON_OTLINE_THICKNESS    5.f
+//#define SHOW_CELLS_COORDINATES    false
+
+
+void Gui::draw(std::vector<std::vector<Object::ObjectType>> result ,WorldSize* w)
+{
+    sf::CircleShape Crc1;
+
+    int x = 0, y = 0, k = 0;
+
+    Crc1.setRadius(20);                                 ///  радиус
+    Crc1.setPointCount(6);                              ///  граница
+    Crc1.setOutlineColor(sf::Color(70, 70, 70));        ///  цвет гр          /* поле */
+    Crc1.setOutlineThickness(3);                        ///  толщина границы 
+    Crc1.setFillColor(sf::Color(120, 120, 120));        ///  цвет внутри
+
+    sf::Event event;
+
+    //while (mWindow.pollEvent(event))
+    //{
+    //    if (event.type == sf::Event::MouseButtonPressed)
+    //    {
+    //        if (event.mouseButton.button == sf::Mouse::Right)
+    //        {
+    //            std::cout << " ASD ";
+    //        }
+    //    }
+    //    if (event.type == sf::Event::KeyReleased)
+    //    {
+    //        switch (event.key.code)
+    //        {
+    //            case (sf::Keyboard::Key::Z):
+    //            {
+    //                bool q = w->make_a_pause;
+    //                w->make_a_pause = !q;
+    //                std::cout << "ASDASD" << std::endl;
+    //                break;
+    //            }
+    //            case (sf::Keyboard::Key::C):
+    //            {
+    //                bool q = w->need_to_draw;
+    //                w->need_to_draw = !q;
+    //                std::cout << "ASDASasD" << std::endl;
+    //                break;
+    //            }
+    //            
+    //        }
+    //        if (event.type == sf::Event::Closed)
+    //            mWindow.close();
+    //        
+    //    }
+    //    //sf::Font font;//шрифт 
+
+    //    //if (!font.loadFromFile("111.ttf"))
+    //    //{
+    //    //    std::cout << "Font load error!\n";
+    //    //}
+
+    //}
+
+
+    sf::View view1;
+    mWindow.clear();
+
+    for (int i = 0; i < result.size()-(w->x_draw); i++) {
+        for (int j = 0; j < result[0].size()-(w->y_draw); j++)
+        {
+
+                if (result[i + (w->x_draw)][j + (w->y_draw)] == Object::ObjectType::VOID)
+                    Crc1.setFillColor(sf::Color(120, 120, 120));    //  пусто + !!!!
+
+
+                if (result[i + (w->x_draw)][j + (w->y_draw)] == Object::ObjectType::BOT)         // бот    +
+                    Crc1.setFillColor(sf::Color(250, 250, 20));
+
+
+                if (result[i + (w->x_draw)][j + (w->y_draw)] == Object::ObjectType::FOOD)        // еда    +
+                    Crc1.setFillColor(sf::Color(12, 180, 12));
+
+
+                if (result[i + (w->x_draw)][j + (w->y_draw)] == Object::ObjectType::POISON)     // яд     
+                    Crc1.setFillColor(sf::Color(250, 0, 0));
+
+
+                if (result[i + (w->x_draw)][j + (w->y_draw)] == Object::ObjectType::WALL)        // стена
+                    Crc1.setFillColor(sf::Color(70, 70, 70));
+
+
+                if (result[i + (w->x_draw)][j + (w->y_draw)] == Object::ObjectType::NUN)         // 0
+                    Crc1.setFillColor(sf::Color(0, 0, 0));
+
+
+
+                if (i % 2 == 1)
+                    x = j * 37+19 ;// +19;
+                
+
+                if (i % 2 == 0)
+                    x = j * 37;
+                
+                Crc1.setPosition(x, y);
+                mWindow.draw(Crc1);
+ #ifdef SHOW_CELLS_COORDINATES
+                 sf::Text text("asdfjkashjkf", font, 20);             //создаем объект текст. закидываем в объект текст строку, шрифт, размер шрифта(в пикселях);//сам объект текст (не строка)
+                 text.setStyle(sf::Text::Bold);                        //жирный и подчеркнутый текст. по умолчанию он "худой":)) и не подчеркнутый
+                 text.setFillColor(sf::Color::White);
+                 text.setPosition(10, 10);                    //задаем позицию текста, центр камеры
+
+                 text.setScale(sf::Vector2f(0.5, 0.5));
+                 text.setPosition(x + 10, y + 10);
+                 text.setString(std::to_string(i) + " " + std::to_string(j));
+                 mWindow.draw(text);
+                 text.setScale(sf::Vector2f(1, 1));
+#endif // SHOW_CELLS_COORDINATES
+            }
+            y = (i+1) * 34;
+        }
+        //  mWindow.draw(text);//рисую этот текст
+        mWindow.display();
+}
+
+//Gui::Gui(sf::RenderWindow& aWindow) :
+Gui::Gui( int x, int y) :
+    mWindow(sf::VideoMode(x, y), "EvOlUtIoN")
+{
+    fin.open("out.out");
+};     ///  поле};
+Gui::~Gui() 
+{
+    fin.close();
+};
+
+std::vector<Gui::EventType> Gui::get_events()
+{
+    std::vector<Gui::EventType> result;
+    sf::Event event;
+    while (mWindow.pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+            mWindow.close();
+        if (event.type == sf::Event::KeyReleased)
+        {
+            switch (event.key.code)
+            {
+                case sf::Keyboard::Key::Z :
+                {
+                    result.push_back(Gui::EventType::SWITCH_PAUSE);
+                    break;
+                }
+                case sf::Keyboard::Key::C :
+                {
+                    result.push_back(Gui::EventType::SWITCH_DRAW_MODE);
+                    break;
+                }
+                case sf::Keyboard::Key::Q :
+                {
+                    result.push_back(Gui::EventType::INCREASE_SPEED);
+                    break;
+                }
+                case sf::Keyboard::Key::W :
+                {
+                    result.push_back(Gui::EventType::DECREASE_SPEED);
+                    break;
+                }
+                case sf::Keyboard::Key::Space :
+                {
+                    result.push_back(Gui::EventType::STANDART_PAUSE);
+                    break;
+                }
+                case sf::Keyboard::Key::G:
+                {
+                    result.push_back(Gui::EventType::DRAW_GRAPH);
+                    break;
+                }
+            }
+        }
+    }
+    return result;
+}
+
+sf::VertexArray Gui::zad()
+{
+    int n = 0, pr = 0;
+    std::vector<int> Vec;
+    fin.open("out.out");
+    while (fin >> n)
+    {
+        
+        fin >> pr;
+        Vec.push_back(pr);
+    }
+    fin.close();
+    int fpr = 0;
+    sf::VertexArray lines(sf::Lines, Vec.size());
+    //lines[0].position = sf::Vector2f(0, 0);
+
+    for (int i = 1; i < Vec.size(); ++i)
+    {
+        lines[i].position = sf::Vector2f(fpr, Vec[i]);
+        fpr = Vec[i];
+        //далее для каждой точки указываем цвет(так можно создавать градиенты):
+        //lines[0].color = sf::Color::White;
+        //lines[1].color = sf::Color::Blue;
+    }
+    return lines;
+};
+
+void Gui::draw_graph(sf::VertexArray asd)
+{
+    
+    mWindow.clear();
+    mWindow.draw(asd);
+    mWindow.display();
+}
